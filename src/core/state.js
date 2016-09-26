ec0c262061b574ef5155372274158868d4f74ae3
Julius Rajala <jijraj@utu.fi>
2016-09-26 19:46:57 +0300

    Removed test data.

--- src/core/state.js
+++ src/core/state.js
@@ -1,6 +1,6 @@
 import { fromJS } from 'immutable';
 
-const testState = {
+const initialState = {
 'activeGuild':'',
 'activeStudent':'',
 'guilds': [
@@ -552,62 +552,5 @@ const testState = {
     ]
   }
 ]}
-
-const initialState = {
-  'activeGuild':'',
-  'activeStudent':'',
-  'guilds': [
-    {
-      'guild': 'Digit',
-      'groups': [
-        {
-          'tutors': ['Kille', 'Pertti'],
-          'students': [
-            {'name':'Juha'},
-            {'name':'Kalle'},
-            {'name':'Heimo'}
-          ]
-        }
-      ]
-    },
-    {
-      'guild': 'Asteriski',
-      'groups': [
-        {
-          'tutors': ['Kalle', 'Pekka'],
-          'students': [
-            {'name':'Jenni'},
-            {'name':'Kiia'},
-            {'name':'Helmi'}
-          ]
-        },
-        {
-          'tutors': ['Kalle', 'Pekka'],
-          'students': [
-            {'name':'Jenni'},
-            {'name':'Kiia'},
-            {'name':'Helmi'}
-          ]
-        },
-        {
-          'tutors': ['Kalle', 'Pekka'],
-          'students': [
-            {'name':'Jenni'},
-            {'name':'Kiia'},
-            {'name':'Helmi'}
-          ]
-        },
-        {
-          'tutors': ['Kalle', 'Pekka'],
-          'students': [
-            {'name':'Jenni'},
-            {'name':'Kiia'},
-            {'name':'Helmi'}
-          ]
-        }
-      ]
-    }
-  ]
-}
 
 export const immutableState = fromJS(initialState);
